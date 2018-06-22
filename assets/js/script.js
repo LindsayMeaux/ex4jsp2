b45039027b9ad039543dbbf7cf882f4ca922488e
lindsaym <lindsay.meaux@gmail.com>
2018-06-22 15:58:59 +0200

    raz js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function resetFileds(){
+document.getElementById("lastname").value="";
+document.getElementById("firstname").value=""
+document.getElementById("city").value=""
+document.getElementById("button").selectedIndex=-1;
+}
